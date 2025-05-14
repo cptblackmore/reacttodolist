922db3f15652fa73769487e576810350c3feb749
cptblackmore <redshatterr@gmail.com>
2025-05-14 13:53:39 +0300

    Install import and prettier eslint plugins, configure eslint to resolve imports and to lint codestyle inconsistencies

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,17 +6,44 @@ module.exports = {
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
+    'plugin:import/recommended',
+    'plugin:prettier/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+  parserOptions: { 
+    ecmaVersion: 'latest', 
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  overrides: [
+    {
+      files: ['*.jsx'],
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+  ],
+  settings: { 
+    react: { version: '18.2' },
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
+      },
+    }
+  },
+  plugins: ['react-refresh', 'prettier'],
   rules: {
     'react/jsx-no-target-blank': 'off',
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
-	"react/no-unknown-property": ['error', { ignore: ['jsx', 'global'] }]
+    'import/no-unresolved': 'error',
+    'react/no-unknown-property': ['error', { ignore: ['jsx', 'global'] }],
+    'prettier/prettier': 'warn',
   },
 }
